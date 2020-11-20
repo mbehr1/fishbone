01d952f56203f1c79ec526252dc1ee95fffb691d
Matthias Behr <mbehr@mcbehr.de>
2020-11-20 23:56:10 +0100

    Fix OnBlurInputBase not reacting to props changes

--- src/webview/src/components/onBlurInputBase.js
+++ src/webview/src/components/onBlurInputBase.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 import InputBase from '@material-ui/core/InputBase';
 
 export default function OnBlurInputBase(props) {
     const [value, setValue] = React.useState(props.value);
+
+    useEffect(() => {
+        setValue(props.value);
+    }, [props.value]);
 
     return (
         <InputBase {...props} value={value} onChange={(event) => setValue(event.target.value)} onBlur={() => {
