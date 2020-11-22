03780f876aa4a87f374b8af72beb63249973dfc8
Matthias Behr <mbehr@mcbehr.de>
2020-11-22 16:57:12 +0100

    add triggerRestQueryDetails

--- src/webview/src/util.js
+++ src/webview/src/util.js
@@ -68,18 +68,66 @@ export function triggerRestQuery(requestStr, jsonPath) {
                 reject(e);
             }
         });
-/*    } else {
-        // e.g. https:// we do directly: (todo check for https://?)
-        return new Promise((resolve, reject) => {
-            console.log(`triggerRestQuery triggering ${JSON.stringify(requestStr)} via fetch`);
-            const username = typeof requestStr === 'object' && requestStr.username ? requestStr.username : undefined;
-            const password = typeof requestStr === 'object' && requestStr.password ? requestStr.password : undefined;
-            let headers = new Headers();
-            if (username && password) {
-                headers.set('Authorization', 'Basic ' + btoa(username + ":" + password));
+}
+
+/**
+ * perform triggerRestRequery and return the processing details
+ * @param {*} dataSourceObj object with source, jsonPath, conv
+ * @returns object with error| result and restQueryResult, jsonPathResult, convResult
+ */
+export async function triggerRestQueryDetails(dataSourceObj) {
+    const answer = {};
+    try {
+        const res = await triggerRestQuery(dataSourceObj.source);
+        answer.restQueryResult = res;
+
+        let result = res;
+        if (dataSourceObj.jsonPath?.length > 0) {
+            result = jp.query(res, dataSourceObj.jsonPath);
+            answer.jsonPathResult = result;
+        }
+        if (dataSourceObj.conv?.length > 0) {
+            const dataConv = dataSourceObj.conv;
+            const indexFirstC = dataConv.indexOf(':');
+            const convType = dataConv.slice(0, indexFirstC);
+            const convParam = dataConv.slice(indexFirstC + 1);
+            //console.log(`convType='${convType}' convParam='${convParam}'`);
+            switch (convType) {
+                case 'length':
+                    answer.convResult = Array.isArray(result) ? result.length : 0;
+                    //console.log(`conv length from ${JSON.stringify(result)} returns '${answer.convResult}'`);
+                    break;
+                case 'index':
+                    answer.convResult = Array.isArray(result) && result.length > Number(convParam) ? (typeof result[Number(convParam)] === 'string' ? result[Number(convParam)] : JSON.stringify(result[Number(convParam)])) : 0;
+                    break;
+                case 'func':
+                    // todo try catch... conv to string/number
+                    try {
+                        // eslint-disable-next-line no-new-func
+                        const fn = new Function("result", convParam);
+                        const fnRes = fn(result);
+                        //console.log(`typeof fnRes='${typeof fnRes}'`);
+                        switch (typeof fnRes) {
+                            case 'string': answer.convResult = fnRes; break;
+                            case 'number': answer.convResult = fnRes; break;
+                            case 'object': answer.convResult = JSON.stringify(fnRes); break;
+                            default:
+                                answer.convResult = `unknown result type '${typeof fnRes}'. Please return string or number`;
+                                break;
+                        }
+                    } catch (e) {
+                        answer.convResult = `got error e='${e}' from conv function`;
+                    }
+                    break;
+                default:
+                    answer.convResult = `unknown convType ${convType}`;
+                    break;
             }
-            fetch(url, { method: 'GET', headers: headers })
-                .then(response => response.json()).then(json => resolve(json)).catch(e => reject(e));
-        });
-    }*/
+        }
+        answer.result = answer.convResult !== undefined ? answer.convResult : (answer.jsonPathResult !== undefined ? answer.jsonPathResult : answer.restQueryResult);
+    } catch (e) {
+        console.log(`triggerRestQueryDetails got error=`, e);
+        answer.error = e && e.errors && Array.isArray(e.errors) ? e.errors.join(' / ') : `unknown error:'${JSON.stringify(e)}'`;
+    }
+    return answer;
 }
