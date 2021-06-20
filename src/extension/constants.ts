96c9f8d9be7230b3a432cd49582a525111c3f909
Matthias Behr <mbehr@mcbehr.de>
2021-06-20 15:08:39 +0200

    fix: add constants.ts

---
+++ src/extension/constants.ts
@@ -0,0 +1,10 @@
+/* --------------------
+* Copyright (C) Matthias Behr, 2021
+*/
+
+export const extensionId = 'mbehr1.fishbone';
+
+export enum GlobalState {
+    Version = 'fishbone:version',
+    PendingWhatNewOnFocus = 'fishbone:pendingWhatsNewOnFocus'
+}
