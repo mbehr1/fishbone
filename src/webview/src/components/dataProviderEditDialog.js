674bd6ba42185b24142bc65c00ab6e76c5877c4d
Matthias Behr <mbehr@mcbehr.de>
2022-03-04 20:35:43 +0100

    fix: typo appy -> apply

--- src/webview/src/components/dataProviderEditDialog.js
+++ src/webview/src/components/dataProviderEditDialog.js
@@ -114,7 +114,7 @@ export default function DataProviderEditDialog(props) {
     return (
         <Dialog fullScreen open={props.open} onClose={handleClose}>
             <DialogTitle id="dpEditDialogTitle">
-                {props.applyMode ? 'Edit appy filter settings...' : 'Edit badge and filter settings...'}
+                {props.applyMode ? 'Edit apply filter settings...' : 'Edit badge and filter settings...'}
                 <IconButton onClick={handleClose} color="inherit" style={{ position: 'absolute', right: 1 }}>
                     <CloseIcon />
                 </IconButton>
